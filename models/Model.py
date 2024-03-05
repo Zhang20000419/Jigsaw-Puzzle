06d42885e2a6a194e4490bf30d07f0763530d67e
ZZC's computer <2532214352@qq.com>
2024-03-05 17:58:46 +0800

    vision1-finish batch normalization

--- models/Model.py
+++ models/Model.py
@@ -7,9 +7,13 @@ class LitNet(nn.Module):
         self.num_labels = num_labels
         self.padding = nn.ZeroPad2d(2)
         self.Conv1 = nn.Conv2d(3, 50, kernel_size=5, stride=2, padding=2, padding_mode='zeros')
+        self.norm1 = nn.BatchNorm2d(50)
         self.Conv2 = nn.Conv2d(50, 100, kernel_size=5, stride=2, padding=2, padding_mode='zeros')
+        self.norm2 = nn.BatchNorm2d(100)
         self.Conv3 = nn.Conv2d(100, 100, kernel_size=3, stride=2, padding=1, padding_mode='zeros')
+        self.norm3 = nn.BatchNorm2d(100)
         self.Conv4 = nn.Conv2d(100, 200, kernel_size=3, stride=1, padding=1, padding_mode='zeros')
+        self.norm4 = nn.BatchNorm2d(200)
         self.relu = nn.ReLU(inplace=True)
 
         self.classifier = nn.Sequential(
@@ -34,7 +38,8 @@ class LitNet(nn.Module):
         x = self.Conv1(x)
         x = self.relu(x)
         # batch normalization
-        # x = nn.BatchNorm2d(x.shape[1])(x)
+        x = self.norm1(x)
+
         # 50*26*26
         x = nn.MaxPool2d(kernel_size=2, stride=2)(x)
 
@@ -42,21 +47,21 @@ class LitNet(nn.Module):
         x = self.Conv2(x)
         x = self.relu(x)
         # batch normalization
-        # x = nn.BatchNorm2d(x.shape[1])(x)
+        x = self.norm2(x)
         x = nn.Dropout(p=0.3)(x)
 
         # 100*7*7
         x = self.Conv3(x)
         x = self.relu(x)
         # batch normalization
-        # x = nn.BatchNorm2d(x.shape[1])(x)
+        x = self.norm3(x)
         x = nn.Dropout(p=0.3)(x)
 
         # 200*7*7
         x = self.Conv4(x)
         x = self.relu(x)
         # batch normalization
-        # x = nn.BatchNorm2d(x.shape[1])(x)
+        x = self.norm4(x)
         x = nn.Dropout(p=0.3)(x)
 
         x = x.contiguous().view(batch_size, -1)
